2a6b29bcedc7977a7bf7bbebe93ed9e5a3791bd9
Alex Corsa <98650988+Alex-Corsa@users.noreply.github.com>
2025-06-26 00:18:26 +0300

    Version 1.0

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,68 +8,23 @@ document.querySelectorAll('div').forEach(div => {
     }
 });
 
-// function initMap() {
-//     const styledMapType = new google.maps.StyledMapType(
-//       [
-//         {
-//           "featureType": "landscape.natural",
-//           "elementType": "geometry.fill",
-//           "stylers": [
-//             { "visibility": "on" },
-//             { "color": "#e0efef" }
-//           ]
-//         },
-//         {
-//           "featureType": "poi",
-//           "elementType": "geometry.fill",
-//           "stylers": [
-//             { "visibility": "on" },
-//             { "hue": "#1900ff" },
-//             { "color": "#c0e8e8" }
-//           ]
-//         },
-//         {
-//           "featureType": "road",
-//           "elementType": "geometry",
-//           "stylers": [
-//             { "lightness": 100 },
-//             { "visibility": "simplified" }
-//           ]
-//         },
-//         {
-//           "featureType": "road",
-//           "elementType": "labels",
-//           "stylers": [
-//             { "visibility": "off" }
-//           ]
-//         },
-//         {
-//           "featureType": "transit.line",
-//           "elementType": "geometry",
-//           "stylers": [
-//             { "visibility": "on" },
-//             { "lightness": 700 }
-//           ]
-//         },
-//         {
-//           "featureType": "water",
-//           "elementType": "all",
-//           "stylers": [
-//             { "color": "#7dcdcd" }
-//           ]
-//         }
-//       ],
-//       { name: "Styled Map" }
-//     );
+document.addEventListener('DOMContentLoaded', function() {
+    const linkElements = document.querySelector('data-link');
 
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//       center: { lat: 50.4501, lng: 30.5234 }, // Київ
-//       zoom: 12,
-//       mapTypeControlOptions: {
-//         mapTypeIds: ["roadmap", "satellite", "styled_map"]
-//       }
-//     });
+    function isMobileView() {
+        return window.matchMedia("(max-width: 768px)").matches;
 
-//     map.mapTypes.set("styled_map", styledMapType);
-//     map.setMapTypeId("styled_map");
-//   }
+    }
+
+    function updateLinkHref() {
+        if (isMobileView()) {
+            linkElements.href = "tel:+48570790975";
+        } else {
+            linkElements.href = "https://t.me/+380988900260";
+        }
+    }
+
+    updateLinkHref();
+
+    window.addEventListener('resize', updateLinkHref);
+});
